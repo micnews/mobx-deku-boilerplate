c8a9ebebf2c6e41336435c0e09357629cd996f7e
Orry Baram <orry@mic.com>
2016-07-01 14:55:46 -0400

    simplified example

--- client/components/Counter.jsx
+++ client/components/Counter.jsx
@@ -3,20 +3,16 @@ import {observer} from 'mobx-deku';
 
 export default observer({
   render ({props}) {
+    const {state} = props;
+
     return (
       <div class='counter'>
-        <div class='counter__display'>{props.state.counter}</div>
+        <div class='counter__display'>{state.counter}</div>
         <div>
-          <button class='button' onClick={decreaseCounter}>-</button>
-          <button class='button' onClick={increaseCounter}>+</button>
+          <button class='button' onClick={state.decreaseCounter.bind(state)}>-</button>
+          <button class='button' onClick={state.increaseCounter.bind(state)}>+</button>
         </div>
       </div>
     );
-    function increaseCounter () {
-      props.state.increaseCounter();
-    }
-    function decreaseCounter () {
-      props.state.decreaseCounter();
-    }
   }
 });
--- client/states/State.js
+++ client/states/State.js
@@ -2,10 +2,11 @@ import {observable, action} from 'mobx';
 
 export default class State {
   @observable counter = 0;
+
   @action increaseCounter () {
     this.counter += 1;
   }
   @action decreaseCounter () {
     this.counter -= 1;
   }
-}
+ }
